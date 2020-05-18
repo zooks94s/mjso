c792b9ba63790598900a0e6256e8186e91c39e70
zooks94s <zooks94s@gmail.com>
2020-05-18 19:28:23 +0900

    global header

---
+++ app/components/GlobalHeader.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+  header.global-header
+    .global-header_brand
+      p
+        nuxt-link(to="/") logo
+      p Scroll
+    .global-header_nav
+      p menu
+      p tel. 03-3388-0022
+</template>
+
+<style lang="scss" scoped>
+.global-header {
+  &_brand {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 140px;
+    height: 100vh;
+  }
+
+  &_nav {
+    position: fixed;
+    top: 0;
+    right: 0;
+    width: 140px;
+    height: 100vh;
+  }
+}
+</style>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,36 +1,27 @@
-<template>
-  <div>
-    <nuxt />
-  </div>
+<template lang="pug">
+  .app-layout
+    GlobalHeader
+    .app-layout_content
+      nuxt
+      .app-layout_bottom
 </template>
 
-<style>
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
+<script>
+import GlobalHeader from '@/components/GlobalHeader.vue'
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
+export default {
+  components: {
+    GlobalHeader,
+  },
 }
+</script>
 
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+<style lang="scss" scoped>
+.app-layout {
+  &_content {
+    width: 100%;
+    padding: 0 140px;
+    margin: 0 auto;
+  }
 }
 </style>
